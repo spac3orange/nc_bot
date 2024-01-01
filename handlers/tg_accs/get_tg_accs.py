c0fe5bad0ca18d2165ad4f37975bf2f6893f9df4
spac3orange <m4gi3k44@proton.me>
2024-01-02 03:09:01 +0600

    version 5.11.2

--- handlers/tg_accs/get_tg_accs.py
+++ handlers/tg_accs/get_tg_accs.py
@@ -27,6 +27,44 @@ async def get_info(accounts: list, uid=None) -> List[Tuple[str]]:
     return accs_info
 
 
+async def send_acc_info(acc, callback, user_id=None) -> None:
+    try:
+        if not user_id:
+            acc_string = ''
+            acc_info = await get_info([acc['phone']])
+            print(acc_info)
+            for phone, id, name, surname, username, restricted, about, sex in acc_info:
+                acc_string += f'<b>Тел:</b> {phone}' \
+                              f'\n<b>ID:</b> {id}' \
+                              f'\n<b>Имя:</b> {name}' \
+                              f'\n<b>Фамилия:</b> {surname}' \
+                              f'\n<b>Пол:</b> {sex}' \
+                              f'\n<b>Ник:</b> @{username}' \
+                              f'\n<b>Био:</b> {about}' \
+                              f'\n<b>Ограничения:</b> {restricted}' \
+                              f'\n<b>Комментарии:</b> {acc["comments"]}'
+                await callback.message.answer(acc_string, parse_mode='HTML')
+                acc_string = ''
+        else:
+            acc_string = ''
+            acc_info = await get_info([acc['phone']], user_id)
+            for phone, id, name, surname, username, restricted, about, sex in acc_info:
+                acc_string += f'<b>Тел:</b> {phone}' \
+                              f'\n<b>ID:</b> {id}' \
+                              f'\n<b>Имя:</b> {name}' \
+                              f'\n<b>Фамилия:</b> {surname}' \
+                              f'\n<b>Пол:</b> {sex}' \
+                              f'\n<b>Ник:</b> @{username}' \
+                              f'\n<b>Био:</b> {about}' \
+                              f'\n<b>Ограничения:</b> {restricted}' \
+                              f'\n<b>Комментарии:</b> {acc["comments"]}'
+                await callback.message.answer(acc_string, parse_mode='HTML')
+                acc_string = ''
+    except Exception as e:
+        logger.error(e)
+        await callback.message.answer(f'Ошибка при запросе информации об аккаунте {acc["phone"]}.'
+                                      f'\nПожалуйста, попробуйте позднее.')
+
 @router.callback_query(F.data == 'tg_accs_status', KnownUser())
 async def get_acc_info(callback: CallbackQuery, state: FSMContext):
     uid = callback.from_user.id
@@ -40,40 +78,14 @@ async def get_acc_info(callback: CallbackQuery, state: FSMContext):
                 if table == 'telegram_accounts':
                     await callback.message.answer('<b>Бесплатные аккаунты:</b>', parse_mode='HTML')
                     for acc in value:
-                        acc_string = ''
-                        acc_info = await get_info([acc['phone']])
-                        print(acc_info)
-                        for phone, id, name, surname, username, restricted, about, sex in acc_info:
-                            acc_string += f'<b>Тел:</b> {phone}' \
-                                          f'\n<b>ID:</b> {id}' \
-                                          f'\n<b>Имя:</b> {name}' \
-                                          f'\n<b>Фамилия:</b> {surname}' \
-                                          f'\n<b>Пол:</b> {sex}' \
-                                          f'\n<b>Ник:</b> @{username}' \
-                                          f'\n<b>Био:</b> {about}' \
-                                          f'\n<b>Ограничения:</b> {restricted}' \
-                                          f'\n<b>Комментарии:</b> {acc["comments"]}'
-                            await callback.message.answer(acc_string, parse_mode='HTML')
-                            acc_string = ''
-                            #hm
+                        await send_acc_info(acc, callback)
+
                 elif table.startswith('accounts_'):
                     user_id = table.split('_')[-1]
                     await callback.message.answer(f'<b>Аккаунты пользователя {table.split("_")[-1]}:</b>', parse_mode='HTML')
                     for acc in value:
-                        acc_string = ''
-                        acc_info = await get_info([acc['phone']], user_id)
-                        for phone, id, name, surname, username, restricted, about, sex in acc_info:
-                            acc_string += f'<b>Тел:</b> {phone}' \
-                                          f'\n<b>ID:</b> {id}' \
-                                          f'\n<b>Имя:</b> {name}' \
-                                          f'\n<b>Фамилия:</b> {surname}' \
-                                          f'\n<b>Пол:</b> {sex}' \
-                                          f'\n<b>Ник:</b> @{username}' \
-                                          f'\n<b>Био:</b> {about}' \
-                                          f'\n<b>Ограничения:</b> {restricted}' \
-                                          f'\n<b>Комментарии:</b> {acc["comments"]}'
-                            await callback.message.answer(acc_string, parse_mode='HTML')
-                            acc_string = ''
+                        await send_acc_info(acc, callback, user_id)
+
         else:
             await callback.message.answer('Нет подключенных аккаунтов.')
 
