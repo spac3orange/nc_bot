a8308ea7394cca126712167e2b91d1d9cbed328b
spac3orange <m4gi3k44@proton.me>
2024-02-11 16:18:50 +0600

    version 1.7

--- data/config_telethon_scheme.py
+++ data/config_telethon_scheme.py
@@ -343,7 +343,7 @@ class TelethonConnect:
                                 continue
                             input_entity = InputPeerChannel(entity.id, entity.access_hash)
                             utc_now = datetime.now(pytz.utc)
-                            offset_date = utc_now - timedelta(minutes=10)
+                            offset_date = utc_now - timedelta(minutes=5)
                             messages = await self.client(GetHistoryRequest(
                                 peer=input_entity,
                                 limit=10,
