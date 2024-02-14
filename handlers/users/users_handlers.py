e6c7f098d38268f2e910cb1077b33a1b30c34a50
spac3orange <m4gi3k44@proton.me>
2024-02-14 15:04:49 +0600

    version 5.16.1

--- handlers/users/users_handlers.py
+++ handlers/users/users_handlers.py
@@ -148,7 +148,7 @@ async def select_balance_operation(callback: CallbackQuery):
     selected_user = callback.data.split('_')[-1]
     print(selected_user)
     await callback.message.answer(f'Выбран пользователь: {selected_user}'
-                                  f'\nВыберите операцию:', kb_admin.balance_ops_btns(selected_user))
+                                  f'\nВыберите операцию:', reply_markup=kb_admin.balance_ops_btns(selected_user))
 
 
 @router.callback_query(F.data.startswith('user_add_balance_'))
